bf07590b72bd958135d5e457ef3de27b738ea25f
Zeina Dajani <dajani.zeina@gmail.com>
2015-07-27 14:43:25 +0100

    added a gitignore file

--- js/gitprofile.js
+++ js/gitprofile.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     e.preventDefault();
 
-    var access_token = '7f0a4ddf575100cf609e2db66c91ae087a3f0f40' // Replace this with a Github access token that you can generate as explained here https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+    var access_token = token; // Replace this with a Github access token that you can generate as explained here https://help.github.com/articles/creating-an-access-token-for-command-line-use/
     var url = 'https://api.github.com/users/' + $('input.username').val() + "?access_token=" + access_token;
 
     var template = $('template').html();
@@ -12,7 +12,7 @@ $(document).ready(function() {
     $.get(url, function(info) {
       $('.container').prepend(Mustache.render(template, info));
     }).fail(function() {
-      $('.container').prepend("User not found")
+      $('.container').prepend("User not found");
     }).always(function() {
       $('input.username').val('');
       });
